8b336e8119410513fabb531aa307db77f9bf1474
saov4000 <saov4000@gmail.com>
2025-11-27 16:34:07 -0300

    2 envio

--- js/script.js
+++ js/script.js
@@ -3,10 +3,38 @@
 let valor1;
 let valor2;
 let resultado;
+let expressao = "";
 
-function calcular() {
+function somar() {
     valor1 = parseInt(document.getElementById("valor1").value);
     valor2 = parseInt(document.getElementById("valor2").value);
     resultado = valor1+valor2;
     document.getElementById("resultado").innerHTML = resultado; 
+}
+
+function subtrair(){
+    valor1 = parseInt(document.getElementById("valor1").value);
+    valor2 = parseInt(document.getElementById("valor2").value);
+    resultado = valor1-valor2;
+    document.getElementById("resultado").innerHTML = resultado; 
+}
+
+function multiplicar(){
+    valor1 = parseInt(document.getElementById("valor1").value);
+    valor2 = parseInt(document.getElementById("valor2").value);
+    resultado = valor1*valor2;
+    document.getElementById("resultado").innerHTML = resultado; 
+}
+
+function dividir(){
+    valor1 = parseInt(document.getElementById("valor1").value);
+    valor2 = parseInt(document.getElementById("valor2").value);
+    resultado = valor1/valor2;
+    document.getElementById("resultado").innerHTML = resultado; 
+}
+
+function pegar(caracter){
+    expressao = expressao+caracter
+    alert(expressao)
+    document.getElementById("valor").value = expressao
 }
